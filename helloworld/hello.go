c14c7ff82dd3325735953967347a3054903167ea
udhos <udhos>
2019-02-28 18:57:06 -0300

    Lazy init.

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -9,20 +9,29 @@ import (
 
 const version = "0.3"
 
+// If you initialize variables in global scope, the initialization code will
+// always be executed via a cold start invocation, increasing your function's latency
+//
+// var cache int = heavyComputation() // DON'T do this
 var cache int
 
 // init runs during package initialization in instance's cold start.
 // see https://cloud.google.com/functions/docs/bestpractices/tips
 func init() {
-        cache = 1000
+	// cache = heavyComputation() // you could do this
+	cache = 1000
 }
 
 func HelloGet(w http.ResponseWriter, r *http.Request) {
 
+	// if needToInitCache {
+	//     cache = heavyComputation() // or you could do this
+	// }
+
 	c := fmt.Sprintf(" (cache=%d) ", cache)
 	cache++
 
-	prefix := "Hello, World! version " + version + c 
+	prefix := "Hello, World! version " + version + c
 
 	var d struct {
 		Name string `json:"name"`
